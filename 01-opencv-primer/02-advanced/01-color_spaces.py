4c29d090b55e6297c16f604bcde0b0c68a415703
ahosnyyy <ahmedhosny0094.ah@gmail.com>
2022-07-08 17:02:38 +0200

    session 1

---
+++ 01-opencv-primer/02-advanced/01-color_spaces.py
@@ -0,0 +1,30 @@
+import cv2 as cv2
+import matplotlib.pyplot as plt
+
+img = cv2.imread('../resources/images/cat.jpg')
+cv2.imshow('Cat', img)
+cv2.waitKey(0)
+
+# BGR to Grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow('Gray', gray)
+cv2.waitKey(0)
+
+# BGR to HSV
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+cv2.imshow('HSV', hsv)
+cv2.waitKey(0)
+
+# BGR to L*a*b
+lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+cv2.imshow('LAB', lab)
+cv2.waitKey(0)
+
+# plt.imshow(img)
+# plt.show()
+
+# BGR to RGB
+rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+cv2.imshow('RGB', rgb)
+cv2.waitKey(0)
+
